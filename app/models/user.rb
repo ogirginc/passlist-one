b75ff34b049ca8f412b0d8b3ac56fa3f38687f5a
Oğulcan Girginç <ogulcan@girginc.com>
2025-01-12 16:46:38 +0300

    Add password validations and digest column to user
    - Add `has_secure_password` to User model with custom validations
    - Add migration to include `password_digest` column in users table
    - Update user fixtures with password digests
    - Add tests for password validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
   validates :name, presence: true
+
+  # Disable default password validations as there is no need for confirmation.
+  has_secure_password validations: false
+  validate :strong_password, if: -> { password.present? }
+
+  MINIMUM_PASSWORD_LENGTH = 10
+  MAXIMUM_PASSWORD_LENGTH = 16
+
+  private
+
+  def strong_password
+    return if password.blank?
+
+    unless password.length.between?(MINIMUM_PASSWORD_LENGTH, MAXIMUM_PASSWORD_LENGTH)
+      errors.add(:password, "must be between #{MINIMUM_PASSWORD_LENGTH} and #{MAXIMUM_PASSWORD_LENGTH} characters")
+    end
+
+    errors.add(:password, "must contain at least one lowercase letter") unless password =~ /[a-z]/
+    errors.add(:password, "must contain at least one uppercase letter") unless password =~ /[A-Z]/
+    errors.add(:password, "must contain at least one digit") unless password =~ /\d/
+    errors.add(:password, "cannot contain three repeating characters") if password =~ /(.)\1\1/
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,62 @@ class UserTest < ActiveSupport::TestCase
     user.name = "     "
     assert_not user.valid?
   end
+
+  class PasswordValidationsTest < ActiveSupport::TestCase
+    def setup
+      @user = users(:one)
+      @user.password = "Validp@ss123"
+    end
+
+    test "should not be too short" do
+      @user.password = "Short1@"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must be between 10 and 16 characters"
+    end
+
+    test "should not be too long" do
+      @user.password = "Toolongp@ssword123"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must be between 10 and 16 characters"
+    end
+
+    test "with valid length should be accepted" do
+      @user.password = "Validp@ss123"
+      assert @user.valid?
+    end
+
+    test "should contain at least one lowercase letter" do
+      @user.password = "PASSWORD123@"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must contain at least one lowercase letter"
+    end
+
+    test "should contain at least one uppercase letter" do
+      @user.password = "password123@"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must contain at least one uppercase letter"
+    end
+
+    test "should contain at least one digit" do
+      @user.password = "Password@xxx"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must contain at least one digit"
+    end
+
+
+    test "should not contain three repeating characters" do
+      @user.password = "PasssWord123"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "cannot contain three repeating characters"
+    end
+
+    test "should collect all error messages" do
+      @user.password = "aaa"
+      assert_not @user.valid?
+      assert_includes @user.errors[:password], "must be between 10 and 16 characters"
+      assert_includes @user.errors[:password], "must contain at least one uppercase letter"
+      assert_includes @user.errors[:password], "must contain at least one digit"
+      assert_includes @user.errors[:password], "cannot contain three repeating characters"
+    end
+  end
 end
